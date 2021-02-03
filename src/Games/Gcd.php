43a2474ce0b396e980d3beedbc940830c8eaa59d
Fret <k@imagespark.ru>
2021-02-03 21:53:41 +0300

    Lint fixes

--- src/Games/Gcd.php
+++ src/Games/Gcd.php
@@ -5,7 +5,7 @@ function generateGcdQuestion(): array
     $value1 = rand(3, 99);
     $value2 = rand(6, 99);
     $correctAnswer = '';
-    
+
     $currentQuestion = $value1 . ' ' . $value2;
     for ($i = round(max($value1, $value2) / 2); $i >= 1; $i--) {
         if ($value1 % $i == 0 && $value2 % $i == 0) {
